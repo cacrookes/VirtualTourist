d14978ff983cedb1ec9e733b8309581a775382b9
Christopher Crookes <cacrookes@gmail.com>
2020-07-27 22:47:17 -0700

    Adding getPhotosByCoordinates endpoint.

--- VirtualTourist/Model/FlickrClient.swift
+++ VirtualTourist/Model/FlickrClient.swift
@@ -11,4 +11,22 @@ import Foundation
 class FlickrClient {
     static let apiKey = Keys.flickrApiKey
     
+    enum Endpoints {
+        static let base = "http://api.flickr.com/services/rest/?format=json"
+        static let apiKeyParam = "&api_key=\(FlickrClient.apiKey)"
+        
+        case getPhotosByCoordinates(Double, Double, Int)
+        
+        var stringValue: String {
+            switch self {
+                
+            case .getPhotosByCoordinates(let latitude, let longitude, let radiusInKM):
+                return Endpoints.base + "&method=flickr.photos.search" + Endpoints.apiKeyParam + "&lat=\(latitude)&lon=\(longitude)&radius=\(radiusInKM)"
+            }
+        }
+        
+        var url: URL {
+            return URL(string: stringValue)!
+        }
+    }
 }
