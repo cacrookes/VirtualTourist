ec9f8d88d399e0829837db296a8d0c1a8312c02f
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 13:47:12 -0700

    Adding struct to store constants like identifiers.

---
+++ VirtualTourist/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  VirtualTourist
+//
+//  Created by Christopher Crookes on 2020-07-28.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct identifiers {
+        static let mapToPhotosSegue = "mapToPhotosSegue"
+    }
+}
