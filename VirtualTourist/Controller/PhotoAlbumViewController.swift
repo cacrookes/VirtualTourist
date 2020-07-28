9a1a7851767c824a48cebec50914038e2c0b0b8b
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 13:29:36 -0700

    Adding PhotoAlbumViewController.

---
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Christopher Crookes on 2020-07-28.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import MapKit
+
+class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate {
+
+    var context: NSPersistentContainer!
+    var latitude: CLLocationDegrees!
+    var longitude: CLLocationDegrees!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func newCollectionPressed(_ sender: Any) {
+    }
+    
+}
