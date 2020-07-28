a0f7fe6789977aa331edfbe25e92e54e8dc33505
Christopher Crookes <cacrookes@gmail.com>
2020-07-27 21:52:39 -0700

    Adding FlickrClient class file to handle Flickr network requests.

---
+++ VirtualTourist/Model/FlickrClient.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Christopher Crookes on 2020-07-27.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient {
+    static let apiKey = Keys.flickrApiKey
+    
+}
