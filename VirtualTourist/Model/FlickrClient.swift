eac03566533f84f89d93c35423fb221df0c59527
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 19:35:07 -0700

    Adding getPhotos function to retrieve photo information from Flickr.

--- VirtualTourist/Model/FlickrClient.swift
+++ VirtualTourist/Model/FlickrClient.swift
@@ -11,14 +11,15 @@ import Foundation
 class FlickrClient {
 
     enum Endpoints {
-        static let base = "http://api.flickr.com/services/rest/?format=json"
+        static let base = "http://api.flickr.com/services/rest/?"
 
         static let params = [
             "api_key=\(Keys.flickrApiKey)",
             "format=json",
             "method=flickr.photos.search",
             "radius=10",
-            "per_page=20"
+            "per_page=20",
+            "extras=url_m"
         ].joined(separator: "&")
         
         case getPhotosByCoordinates(Double, Double, Int)
@@ -34,5 +35,33 @@ class FlickrClient {
         var url: URL {
             return URL(string: stringValue)!
         }
+    }
+    
+    class func getPhotos(forLatitude latitude: Double,
+                         forLongitude longitude: Double,
+                         forPageNumber pageNum: Int,
+                         completion: @escaping (FlickrSearchResponse?, Error?) -> Void){
+        let url = Endpoints.getPhotosByCoordinates(latitude, longitude, pageNum).url
+        let task = URLSession.shared.dataTask(with: url) { (data, responese, error) in
+            guard let safeData = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            let newData = safeData.subdata(in: 14..<safeData.count).dropLast()
+            do {
+                let searchResponse = try decoder.decode(FlickrSearchResponse.self, from: newData)
+                DispatchQueue.main.async {
+                    completion(searchResponse, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
     }
 }
