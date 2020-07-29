294b1763fc46685b3a930e205f91ca4352fe49e2
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 18:30:11 -0700

    Cleaning up parameters.

--- VirtualTourist/Model/FlickrClient.swift
+++ VirtualTourist/Model/FlickrClient.swift
@@ -9,22 +9,28 @@
 import Foundation
 
 class FlickrClient {
-    static let apiKey = Keys.flickrApiKey
-    
+
     enum Endpoints {
         static let base = "http://api.flickr.com/services/rest/?format=json"
-        static let apiKeyParam = "&api_key=\(FlickrClient.apiKey)"
+
+        static let params = [
+            "api_key=\(Keys.flickrApiKey)",
+            "format=json",
+            "method=flickr.photos.search",
+            "radius=10",
+            "per_page=20"
+        ].joined(separator: "&")
         
-        case getPhotosByCoordinates(Double, Double, Int, Int)
-        
+        case getPhotosByCoordinates(Double, Double, Int)
+
         var stringValue: String {
             switch self {
-                
-            case .getPhotosByCoordinates(let latitude, let longitude, let radiusInKM, let pageNum):
-                return Endpoints.base + "&method=flickr.photos.search" + Endpoints.apiKeyParam + "&lat=\(latitude)&lon=\(longitude)&radius=\(radiusInKM)&per_page=20&page=\(pageNum)"
+
+            case .getPhotosByCoordinates(let latitude, let longitude, let pageNum):
+                return Endpoints.base + Endpoints.params + "&lat=\(latitude)&lon=\(longitude)&page=\(pageNum)"
             }
         }
-        
+
         var url: URL {
             return URL(string: stringValue)!
         }
