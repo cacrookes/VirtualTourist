31b8431fe76facaea3670d03fda4059b8515f451
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 17:28:47 -0700

    Adding Core Data functionality.

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import CoreData
 import MapKit
 
-class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate {
+class PhotoAlbumViewController: UIViewController {
 
     var container: NSPersistentContainer!
     var pin: Pin!
+    var fetchedResultsController: NSFetchedResultsController<Photo>!
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mapView: MKMapView!
@@ -28,6 +29,20 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate {
     @IBAction func newCollectionPressed(_ sender: Any) {
     }
     
+    fileprivate func setupFetchedResultController() {
+        let fetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "pin == %@", pin)
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: container.viewContext, sectionNameKeyPath: nil, cacheName: "\(pin.latitude)-\(pin.longitude)-photos")
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed.")
+        }
+    }
+    
+    
     func setupMap(){
         let coordinates = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
         let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
@@ -35,6 +50,56 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate {
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinates
         mapView.addAnnotation(annotation)
+    }
+        
+}
+
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let photo = fetchedResultsController.object(at: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.identifiers.photoAlbumCollectionViewCell, for: indexPath) as! PhotoAlbumCollectionViewCell
+        if let photoImage = photo.image {
+            cell.imageView.image = UIImage(data: photoImage)
+        }
+        return cell
+    }
+    
+    
+}
+
+extension PhotoAlbumViewController:NSFetchedResultsControllerDelegate {
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            collectionView.insertItems(at: [newIndexPath!])
+            break
+        case .delete:
+            collectionView.deleteItems(at: [indexPath!])
+            break
+        case .update:
+            collectionView.reloadItems(at: [indexPath!])
+        case .move:
+            collectionView.moveItem(at: indexPath!, to: newIndexPath!)
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        let indexSet = IndexSet(integer: sectionIndex)
+        switch type {
+        case .insert: collectionView.insertSections(indexSet)
+        case .delete: collectionView.deleteSections(indexSet)
+        case .update, .move:
+            fatalError("Invalid change type in controller(_:didChange:atSectionIndex:for:). Only .insert or .delete should be possible.")
+        @unknown default:
+            fatalError("Invalid change type in controller(_:didChange:atSectionIndex:for:). Only .insert or .delete should be possible.")
+        }
     }
     
     
