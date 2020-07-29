e1dd47cb4576b81d7c7431e4b3fe39f0402752c2
Christopher Crookes <cacrookes@gmail.com>
2020-07-28 17:16:56 -0700

    Adding PhotoAlbumCollectionViewCell class file.

---
+++ VirtualTourist/Controller/PhotoAlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Christopher Crookes on 2020-07-28.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
