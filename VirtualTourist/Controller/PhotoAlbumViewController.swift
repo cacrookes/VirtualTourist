97d31568c081df8dd74dd2cc7fd032070229b4cd
Christopher Crookes <cacrookes@gmail.com>
2020-07-29 18:22:51 -0700

    Adding flow layout logic.

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -18,22 +18,58 @@ class PhotoAlbumViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mapView: MKMapView!
-    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.dataSource = self
         setupMap()
+        setupFetchedResultController()
+        
+        // check if there are photos stored for this pin. If not, grab some.
+        if fetchedResultsController.sections?[0].numberOfObjects == 0 {
+           fetchPhotos()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setFlowLayout()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        fetchedResultsController = nil
     }
     
     @IBAction func newCollectionPressed(_ sender: Any) {
     }
     
+    func setFlowLayout() {
+        let space:CGFloat = 3.0
+        
+        print("setting flow layout")
+        
+        // set the dimensions of the cell so we get 3 columns in portrait, or 3 rows in landscape
+        let viewWidth = view.frame.size.width
+        let viewHeight = view.frame.size.height
+        let shortSize = viewWidth < viewHeight ? view.frame.size.width : view.frame.size.height
+        let dimension = (shortSize - (2 * space)) / 3.0
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
     fileprivate func setupFetchedResultController() {
         let fetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "pin == %@", pin)
-        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+                
         fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: container.viewContext, sectionNameKeyPath: nil, cacheName: "\(pin.latitude)-\(pin.longitude)-photos")
+        fetchedResultsController.delegate = self
         
         do {
             try fetchedResultsController.performFetch()
@@ -42,6 +78,37 @@ class PhotoAlbumViewController: UIViewController {
         }
     }
     
+    func fetchPhotos() {
+        FlickrClient.getPhotos(forLatitude: pin.latitude, forLongitude: pin.longitude, forPageNumber: 1) { (flickrSearchResponse, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            guard let response = flickrSearchResponse else { return }
+            for photo in response.photos.photo {
+                let newPhoto = Photo(context: self.container.viewContext)
+                newPhoto.pin = self.pin
+                newPhoto.id = Int32(photo.id) ?? 0
+                if let imageURL = URL(string: photo.url_m){
+                    DispatchQueue.main.async {
+                        newPhoto.image = try? Data(contentsOf: imageURL)
+                        do {
+                            try self.container.viewContext.save()
+                        } catch {
+                             print("Error saving photo: \(error)")
+                        }
+                    }
+                }
+                do {
+                    try self.container.viewContext.save()
+                } catch {
+                     print("Error saving photo: \(error)")
+                }
+                //self.collectionView.reloadData()
+            }
+            //self.collectionView.reloadData()
+        }
+    }
     
     func setupMap(){
         let coordinates = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
@@ -56,19 +123,19 @@ class PhotoAlbumViewController: UIViewController {
 
 extension PhotoAlbumViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        let numResults = fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        return numResults
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let photo = fetchedResultsController.object(at: indexPath)
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.identifiers.photoAlbumCollectionViewCell, for: indexPath) as! PhotoAlbumCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.Identifiers.photoAlbumCollectionViewCell, for: indexPath) as! PhotoAlbumCollectionViewCell
+        cell.imageView.image = #imageLiteral(resourceName: "placeholder")
         if let photoImage = photo.image {
             cell.imageView.image = UIImage(data: photoImage)
         }
         return cell
     }
-    
-    
 }
 
 extension PhotoAlbumViewController:NSFetchedResultsControllerDelegate {
